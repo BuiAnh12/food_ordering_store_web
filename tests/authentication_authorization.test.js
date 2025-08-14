695cdea81f00774e19b3d398676255eb9bd35d15
BuiAnh12 <buianh120403@gmail.com>
2025-08-14 09:31:47 +0700

    feat: create auth - order test

--- tests/authentication_authorization.test.js
+++ tests/authentication_authorization.test.js
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 import { login } from "./utils/helpers";
 
 // Happy case
-test("TC01 - Login with valid credentials", async ({ page }) => {
+test("TC01 - Đăng nhập với tài khoản hợp lệ", async ({ page }) => {
     await login(page, "buianh120403@gmail.com", "123");
     await expect(page.getByText("Đăng nhập thành công!")).toBeVisible();
     await page.waitForTimeout(1000);
@@ -11,7 +11,7 @@ test("TC01 - Login with valid credentials", async ({ page }) => {
 });
 
 // Login with wrong credentials
-test("TC02 - Login with wrong credentials", async ({ page }) => {
+test("TC02 - Đăng nhập với tài khoản không hợp lệ", async ({ page }) => {
     await login(page, "buianh120403@gmail.com", "1231");
     await expect(page.getByText("Email hoặc mật khẩu không hợp")).toBeVisible();
     await page.waitForTimeout(1000);
@@ -19,21 +19,21 @@ test("TC02 - Login with wrong credentials", async ({ page }) => {
 });
 
 // Login with blocked account
-test("TC03 - Login with blocked account", async ({ page }) => {
+test("TC03 - Đăng nhập với tài khoản cửa hàng bị block", async ({ page }) => {
     await login(page, "block@gmail.com", "123");
     await page.waitForTimeout(1000);
     await expect(page.url()).toContain("/auth/blocked");
 });
 
 // Login with pending account
-test("TC04 - Login with pending account", async ({ page }) => {
+test("TC04 - Đăng nhập với tài khoản cửa hàng đang chờ duyệt", async ({ page }) => {
     await login(page, "pending@gmail.com", "123");
     await page.waitForTimeout(1000);
     await expect(page.url()).toContain("/auth/verification-pending");
 });
 
 // Login as staff but access owner-only page
-test("TC05 - Staff tries to access owner-only page", async ({ page }) => {
+test("TC05 - Đăng nhập với vai trò staff nhưng cố truy cập các trang vai trò cao hơn", async ({ page }) => {
     await login(page, "tom@gmail.com", "123");
     await page.waitForTimeout(1000);
     await page.goto("http://localhost:3000/staff"); // Or use navigation as needed
@@ -44,7 +44,7 @@ test("TC05 - Staff tries to access owner-only page", async ({ page }) => {
 });
 
 // Login as owner and access all pages
-test("TC06 - Owner accesses all pages", async ({ page }) => {
+test("TC06 - Đăng nhập với vai trò là owner và có thể truy cập các trang", async ({ page }) => {
     await login(page, "buianh120403@gmail.com", "123");
     await page.waitForTimeout(1000);
     await page.goto("http://localhost:3000/staff");
